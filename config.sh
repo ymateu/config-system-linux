0534637374639f82752c86f8e8b107587eecd233
Mateus A. da Silva <79421993+mateusantonioofc@users.noreply.github.com>
2024-11-24 23:29:19 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,18 +1,13 @@
 # Configure swap
 
-# Install basics
-sudo pacman -Sy nano curl git -y
+# Install basics Manjaro
+# sudo pacman -Sy nano curl git -y
 
-# Install snap
-sudo pacman -S snapd
-sudo systemctl enable --now snapd.socket
-sudo ln -s /var/lib/snapd/snap /snap
+# Install basics Ubunto
+sudo apt install nano curl git vim -y
 
 # Install vscde
 sudo snap install code --classic
-
-# Install Intellij idea
-sudo snap install intellij-idea-community --classic
 
 # Install zsh
 sudo pacman -Sy zsh
@@ -28,33 +23,18 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
 
 # Add powerlevel10k in ~/.zshrc
-ZSH_THEME=powerlevel10k/powerlevel10k
+# ZSH_THEME=powerlevel10k/powerlevel10k
 
 # Config plugins in ~/.zshrc
-plugins=(git zsh-syntax-highlighting zsh-autosuggestions)
+# plugins=(git zsh-syntax-highlighting zsh-autosuggestions)
 
 # Config powerlevel10k
-p10k configure
+# p10k configure
 
 # Configure git
 git config --global user.email "mateusantonioofc@gmail.com"
 git config --global user.name "Mateus"
 
 # End
-sudo pacman -Syu
 source ~/.zshrc
 
-sudo apt install python3.10-venv
-
-# criar e ativar ambiente virtual python
-python3 -m venv venv
-source venv/bin/activate
-
-# desativar ambiente virtual
-deactivate
-
-# instalar dependencias
-pip install -r requirements.txt  
-
-# salvar dependencias
-pip freeze > requirements.txt
