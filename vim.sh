54f92a1671d22964a49539123a16fe4d06d4381c
Mateus A. da Silva <79421993+mateusantonioofc@users.noreply.github.com>
2024-11-24 23:31:44 -0300

    YOUCOMPLETEME

--- vim.sh
+++ vim.sh
@@ -3,3 +3,10 @@ git clone --depth=1 https://github.com/terroo/fonts
 cd fonts
 mv fonts ~/.local/share
 fc-cache -fv
+
+# YOUCOMPLETEME
+apt install build-essential cmake vim-nox python3-dev gcc g++ make
+
+# PATH YOUCOMPLETEME
+cd .vim/plugged/YouCompleteMe  
+python3 install.py --clangd-completer
