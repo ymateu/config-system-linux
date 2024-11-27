8f7f148a30c9e204589215a352b97a6b8562e070
Mateus A. da Silva <79421993+mateusantonioofc@users.noreply.github.com>
2024-11-27 01:18:52 -0300

    Update configure.sh

--- vim/configure.sh
+++ vim/configure.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-# Atualizar e instalar pacotes básicos
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y vim curl git gcc g++ clang clang-format python3-pip build-essential
+function print_message {
+    echo -e "\n==============================="
+    echo "$1"
+    echo "===============================\n"
+}
 
-# Instalar gerenciador de plugins Vim
+print_message "Instalando o gerenciador de pacotes Vim-Plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Instalar FZF (fuzzy finder)
+print_message "Instalando FZF"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
 
-# Instalar clangd (para C/C++ LSP)
-sudo apt install -y clangd
-
-# Configurar o Coc.nvim (extensão para C/C++)
+print_message "Instalando o Coc.nvim (extensão para LSP)"
 vim -c 'PlugInstall | q | q'
 vim -c 'CocInstall coc-clangd | q | q'
+
+print_message "Instalando o clangd para C/C++"
+sudo apt install -y clangd
+
+print_message "Instalando a fonte Nerd Font Hack"
+mkdir -p ~/.local/share/fonts
+curl -fLo ~/.local/share/fonts/Hack-Regular.ttf https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Hack.zip
+cd ~/.local/share/fonts
+unzip Hack.zip
+fc-cache -fv
+
+print_message "Instalação concluída! Reinicie o Vim para carregar a configuração."
